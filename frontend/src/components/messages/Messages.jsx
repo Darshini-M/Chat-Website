aa28deda1fd349f1f8364330277d05de3e587b94
mdarshini <mdarshini2003@gmail.com>
2025-07-29 16:02:11 +0530

    2nd part done

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
+import { set } from "mongoose";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      if (!loading && messages.length > 0) {
+        lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100);
+  }, [messages, loading]);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 };
@@ -17,8 +36,6 @@ const Messages = () => {
 export default Messages;
 
 // STARTER CODE FOR THIS FILE
-
-// import Message from "./Message";
 
 // const Messages = () => {
 //   return (
